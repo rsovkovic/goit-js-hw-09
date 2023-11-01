3b9f98de3b6d0225cb9e97b49f8b28b219c0e7ff
rsovkovic <rsovkovic@gmail.com>
2023-11-01 18:20:19 +0200

    nextCommit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,14 +2,14 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
 form.addEventListener('submit', handleSubmit);
-let formStep;
+
 function handleSubmit(event) {
   event.preventDefault();
   const delay = Number(form.elements.delay.value);
-  let formStep = Number(form.elements.step.value);
+  const formStep = Number(form.elements.step.value);
   const position = form.elements.amount.value;
   let currentDelay = delay;
-  console.log(formStep);
+  // console.log(formStep);
   for (let i = 1; i <= position; i++) {
     createPromise(i, currentDelay)
       .then(({ position, delay }) => {
